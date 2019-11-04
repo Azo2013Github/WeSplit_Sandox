99c0a8304fa3d7d0a3db5d3c7864bde0d4d8a027
Ami <foreverazo2013@gmail.com>
2019-11-04 14:10:52 +0100

    Initial Commit

---
+++ WeSplit_Sandox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit_Sandox
+//
+//  Created by Mati on 04/11/2019.
+//  Copyright © 2019 Mati. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
