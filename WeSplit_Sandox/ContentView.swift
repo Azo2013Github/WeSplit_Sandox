d72fd8e90bdfda138051202cdba77e53a853d599
Ami <foreverazo2013@gmail.com>
2019-11-05 12:03:05 +0100

    The colors of frame app

--- WeSplit_Sandox/ContentView.swift
+++ WeSplit_Sandox/ContentView.swift
@@ -8,10 +8,48 @@
 
 import SwiftUI
 
+struct UsingStacksToArrangeViews: View {
+    var body: some View {
+      VStack(alignment: .leading,spacing: 20) {
+        Text("Hello World")
+        HStack(spacing: 60) {
+          Text("First Text View")
+          ZStack() {
+            Text("<<            >>")
+            Text("Second Text View")
+          }
+        }
+        Spacer()
+      }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        //UsingStacksToArrangeViews()
+//        ZStack {
+////            Color.blue.edgesIgnoringSafeArea(.all)
+//            LinearGradient(gradient: Gradient(colors: [.purple,                      .white]), startPoint:  .top, endPoint: .bottom)
+//            LinearGradient(gradient: Gradient(colors: [.white, .green]), startPoint: .bottom, endPoint: .top)
+            // Cette partie du code c'est pour donner les coleurs au centre du mobile.
+//            RadialGradient(gradient: Gradient(colors: [.blue, .black]),               center: .center, startRadius: 20, endRadius: 200)
+//            AngularGradient(gradient: Gradient(colors: [.green, .blue,                .orange, .purple, .red]), center: .topLeading)
+////        }
+        Button(action : {
+            print("Add to cart was tapped")
+        }) {
+            VStack(spacing: 20){
+
+                Image(systemName: "cart")
+                Text("Add to cart")
+            }
+            .font(.largeTitle)
+            .foregroundColor(.orange)
+        }
+      
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
